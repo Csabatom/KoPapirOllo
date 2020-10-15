f0486b6c2ebd547929d5a200dee2f4c7eb8b3915
Nyirom2 <nyirocsabi@gmail.com>
2020-10-15 16:33:53 +0200

    Valaszthato Targy

--- app/src/main/java/nyiro/csaba/kopapirollo/MainActivity.java
+++ app/src/main/java/nyiro/csaba/kopapirollo/MainActivity.java
@@ -3,12 +3,48 @@ package nyiro.csaba.kopapirollo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button ko;
+    private Button papir;
+    private Button ollo;
+    private ImageView sajatValasztott;
+    private ImageView gepValasztott;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.ko = findViewById(R.id.BTN_Ko);
+        this.papir = findViewById(R.id.BTN_Papir);
+        this.ollo = findViewById(R.id.BTN_Ollo);
+        this.sajatValasztott = findViewById(R.id.IMG_SajatValasztott);
+        this.gepValasztott = findViewById(R.id.IMG_GepValasztott);
+
+        ko.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sajatValasztott.setImageResource(R.drawable.rock);
+            }
+        });
+
+        papir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sajatValasztott.setImageResource(R.drawable.paper);
+            }
+        });
+
+        ollo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sajatValasztott.setImageResource(R.drawable.scissors);
+            }
+        });
     }
 }
