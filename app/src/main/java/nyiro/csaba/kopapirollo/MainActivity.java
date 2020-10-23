ef1d27e3603b0c5150167afd25688fa9cd311bea
Nyirom2 <nyirocsabi@gmail.com>
2020-10-23 12:32:42 +0200

    Final product

--- app/src/main/java/nyiro/csaba/kopapirollo/MainActivity.java
+++ app/src/main/java/nyiro/csaba/kopapirollo/MainActivity.java
@@ -1,26 +1,42 @@
 package nyiro.csaba.kopapirollo;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button ko;
-    private Button papir;
-    private Button ollo;
+    private ImageButton ko;
+    private ImageButton papir;
+    private ImageButton ollo;
+    private ImageView gepSziv1;
+    private ImageView gepSziv2;
+    private ImageView gepSziv3;
+    private ImageView jatekosSziv1;
+    private ImageView jatekosSziv2;
+    private ImageView jatekosSziv3;
     private ImageView sajatValasztott;
     private ImageView gepValasztott;
     private Random rnd;
     private TextView pontok;
     private int sajatNyeresPont;
     private int gepNyeresPont;
+    private TextView dontetlenText;
+    private int dontetlen;
+    private Toast toast;
+    private AlertDialog felugroUzenet;
+    private AlertDialog.Builder felugroUzenetbuilder;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +48,41 @@ public class MainActivity extends AppCompatActivity {
         this.ollo = findViewById(R.id.BTN_Ollo);
         this.sajatValasztott = findViewById(R.id.IMG_SajatValasztott);
         this.gepValasztott = findViewById(R.id.IMG_GepValasztott);
-        this.pontok = findViewById(R.id.pontok);
+        this.gepSziv1 = findViewById(R.id.gepSziv1);
+        this.gepSziv2 = findViewById(R.id.gepSziv2);
+        this.gepSziv3 = findViewById(R.id.gepSziv3);
+        this.jatekosSziv1 = findViewById(R.id.jatekosSziv1);
+        this.jatekosSziv2 = findViewById(R.id.jatekosSziv2);
+        this.jatekosSziv3 = findViewById(R.id.jatekosSziv3);
+        this.dontetlenText = findViewById(R.id.dontetlenText);
+        dontetlen++;
+        felugroUzenetbuilder = new AlertDialog.Builder(MainActivity.this);
+        felugroUzenetbuilder.setMessage("Szeretnél új játékot?")
+                .setPositiveButton("Igen", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        sajatNyeresPont = 0;
+                        gepNyeresPont = 0;
+                        dontetlen = 0;
+                        gepSziv1.setImageResource(R.drawable.heart2);
+                        gepSziv2.setImageResource(R.drawable.heart2);
+                        gepSziv3.setImageResource(R.drawable.heart2);
+                        jatekosSziv1.setImageResource(R.drawable.heart2);
+                        jatekosSziv2.setImageResource(R.drawable.heart2);
+                        jatekosSziv3.setImageResource(R.drawable.heart2);
+                        dontetlenText.setText("Döntetlenek száma: 0");
+                    }
+                })
+                .setNegativeButton("Nem", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                        dialog.cancel();
+                    }
+                })
+                .setTitle("Játék vége")
+                .setCancelable(false);
+        felugroUzenet = felugroUzenetbuilder.create();
 
         rnd = new Random();
 
@@ -45,18 +95,53 @@ public class MainActivity extends AppCompatActivity {
                     case 0:
                         gepValasztott.setImageResource(R.drawable.rock);
                         // DÖNTETLEN
+                        dontetlen++;
+                        dontetlenText.setText("Döntetlenek száma: " + dontetlen);
+                        Toast.makeText(MainActivity.this,"Döntetlen", Toast.LENGTH_SHORT).show();
                         break;
                     case 1:
                         gepValasztott.setImageResource(R.drawable.paper);
                         // NYERT A GÉP
                         gepNyeresPont++;
-                        pontok.setText("Eredmény: Ember: " + sajatNyeresPont + " Computer: " + gepNyeresPont);
+                        if(gepNyeresPont == 3) {
+                            // MUTASD A DIALOGOT
+                            felugroUzenet.show();
+                        } else {
+                            Toast.makeText(MainActivity.this,"Nyert a gép!", Toast.LENGTH_SHORT).show();
+                        }
+                        switch (gepNyeresPont) {
+                            case 1:
+                                jatekosSziv3.setImageResource(R.drawable.heart1);
+                                break;
+                            case 2:
+                                jatekosSziv2.setImageResource(R.drawable.heart1);
+                                break;
+                            case 3:
+                                jatekosSziv1.setImageResource(R.drawable.heart1);
+                                break;
+                        }
                         break;
                     case 2:
                         gepValasztott.setImageResource(R.drawable.scissors);
                         // NYERT A JÁTÉKOS
                         sajatNyeresPont++;
-                        pontok.setText("Eredmény: Ember: " + sajatNyeresPont + " Computer: " + gepNyeresPont);
+                        if(sajatNyeresPont == 3) {
+                            // MUTASD A DIALOGOT
+                            felugroUzenet.show();
+                        } else {
+                            Toast.makeText(MainActivity.this,"Nyert a játékos!", Toast.LENGTH_SHORT).show();
+                        }
+                        switch (sajatNyeresPont) {
+                            case 1:
+                                gepSziv3.setImageResource(R.drawable.heart1);
+                                break;
+                            case 2:
+                                gepSziv2.setImageResource(R.drawable.heart1);
+                                break;
+                            case 3:
+                                gepSziv1.setImageResource(R.drawable.heart1);
+                                break;
+                        }
                         break;
                 }
             }
@@ -72,17 +157,52 @@ public class MainActivity extends AppCompatActivity {
                         gepValasztott.setImageResource(R.drawable.rock);
                         // NYERT A JÁTÉKOS
                         sajatNyeresPont++;
-                        pontok.setText("Eredmény: Ember: " + sajatNyeresPont + " Computer: " + gepNyeresPont);
+                        if(sajatNyeresPont == 3) {
+                            // MUTASD A DIALOGOT
+                            felugroUzenet.show();
+                        } else {
+                            Toast.makeText(MainActivity.this,"Nyert a játékos!", Toast.LENGTH_SHORT).show();
+                        }
+                        switch (sajatNyeresPont) {
+                            case 1:
+                                gepSziv3.setImageResource(R.drawable.heart1);
+                                break;
+                            case 2:
+                                gepSziv2.setImageResource(R.drawable.heart1);
+                                break;
+                            case 3:
+                                gepSziv1.setImageResource(R.drawable.heart1);
+                                break;
+                        }
                         break;
                     case 1:
                         gepValasztott.setImageResource(R.drawable.paper);
                         // DÖNTETLEN
+                        dontetlen++;
+                        dontetlenText.setText("Döntetlenek száma: " + dontetlen);
+                        Toast.makeText(MainActivity.this,"Döntetlen", Toast.LENGTH_SHORT).show();
                         break;
                     case 2:
                         gepValasztott.setImageResource(R.drawable.scissors);
                         // NYERT A GÉP
                         gepNyeresPont++;
-                        pontok.setText("Eredmény: Ember: " + sajatNyeresPont + " Computer: " + gepNyeresPont);
+                        if(gepNyeresPont == 3) {
+                            // MUTASD A DIALOGOT
+                            felugroUzenet.show();
+                        } else {
+                            Toast.makeText(MainActivity.this,"Nyert a gép!", Toast.LENGTH_SHORT).show();
+                        }
+                        switch (gepNyeresPont) {
+                            case 1:
+                                jatekosSziv3.setImageResource(R.drawable.heart1);
+                                break;
+                            case 2:
+                                jatekosSziv2.setImageResource(R.drawable.heart1);
+                                break;
+                            case 3:
+                                jatekosSziv1.setImageResource(R.drawable.heart1);
+                                break;
+                        }
                         break;
                 }
             }
@@ -98,17 +218,52 @@ public class MainActivity extends AppCompatActivity {
                         gepValasztott.setImageResource(R.drawable.rock);
                         // NYERT A GÉP
                         gepNyeresPont++;
-                        pontok.setText("Eredmény: Ember: " + sajatNyeresPont + " Computer: " + gepNyeresPont);
+                        if(gepNyeresPont == 3) {
+                            // MUTASD A DIALOGOT
+                            felugroUzenet.show();
+                        } else {
+                            Toast.makeText(MainActivity.this,"Nyert a gép!", Toast.LENGTH_SHORT).show();
+                        }
+                        switch (gepNyeresPont) {
+                            case 1:
+                                jatekosSziv3.setImageResource(R.drawable.heart1);
+                                break;
+                            case 2:
+                                jatekosSziv2.setImageResource(R.drawable.heart1);
+                                break;
+                            case 3:
+                                jatekosSziv1.setImageResource(R.drawable.heart1);
+                                break;
+                        }
                         break;
                     case 1:
                         gepValasztott.setImageResource(R.drawable.paper);
                         // NYERT A JÁTÉKOS
                         sajatNyeresPont++;
-                        pontok.setText("Eredmény: Ember: " + sajatNyeresPont + " Computer: " + gepNyeresPont);
+                        if(sajatNyeresPont == 3) {
+                            // MUTASD A DIALOGOT
+                            felugroUzenet.show();
+                        } else {
+                            Toast.makeText(MainActivity.this,"Nyert a játékos!", Toast.LENGTH_SHORT).show();
+                        }
+                        switch (sajatNyeresPont) {
+                            case 1:
+                                gepSziv3.setImageResource(R.drawable.heart1);
+                                break;
+                            case 2:
+                                gepSziv2.setImageResource(R.drawable.heart1);
+                                break;
+                            case 3:
+                                gepSziv1.setImageResource(R.drawable.heart1);
+                                break;
+                        }
                         break;
                     case 2:
                         gepValasztott.setImageResource(R.drawable.scissors);
                         // DÖNTETLEN
+                        dontetlen++;
+                        dontetlenText.setText("Döntetlenek száma: " + dontetlen);
+                        Toast.makeText(MainActivity.this,"Döntetlen", Toast.LENGTH_SHORT).show();
                         break;
                 }
             }
